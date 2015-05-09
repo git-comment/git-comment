083e9e8dbd984bed052f14b4d66e10a673e91552
Delisa Mason <iskanamagus@gmail.com>
2015-05-09 09:49:07 -0700

    Add utils

--- src/git-comment.go
+++ src/git-comment.go
@@ -3,7 +3,10 @@ package main
 import (
   "time"
   "github.com/wayn3h0/go-uuid"
+  // "gopkg.in/libgit2/git2go.v22"
 )
+
+type Identifier uuid.UUID
 
 type Person struct {
   Name string
@@ -21,17 +24,64 @@ type Comment struct {
   Content string
   Amender Person
   AmendTime time.Time
-  Commit uuid.UUID
-  ID uuid.UUID
+  Commit Identifier
+  ID Identifier
 }
 
+// Creates a person from the email address and name in the
+// active git config
 func CurrentPerson() *Person {
   return &Person{}
 }
 
+// Finds a comment by a given ID
+func CommentByID(ID Identifier) *Comment{
+
+}
+
+// Finds all comments on a given commit
+func CommentsOnCommit(commit Identifier) []*Comment{
+
+}
+
+// Creates a new comment using provided content and author
 func NewComment(message string, author *Person) *Comment{
   return &Comment{}
 }
+
+// Write git object for a given comment and update the
+// comment refs
+func WriteCommentToDisk(comment *Comment) {
+
+}
+
+// Generate content of git object for comment
+// Comment ref file format:
+//
+//   commit 0155eb4229851634a0f03eb265b69f5a2d56f341
+//   file src/example.txt:12
+//   author Delisa Mason <name@example.com>
+//   1243040974 -0900
+//   amender Delisa Mason <name@example.com>
+//   1243040974 -0900
+//
+//   Too many levels of indentation here.
+//
+func (comment *Comment)ObjectContent() string {
+  return ""
+}
+
+// Generate the path within refs for a given comment
+//
+// Comment refs are nested under refs/comments. The
+// directory name is the first four characters of the
+// commit identifier, and the file name are the
+// remaining characters. The contents of the file are
+// the identifiers of all comments on the commit
+func (comment *Comment)RefPath() string {
+  return ""
+}
+
 
 func main() {
 
