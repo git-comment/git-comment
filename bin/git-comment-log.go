0cd606f8aad46fc19e39f2a98b5477bc2d1263ab
Delisa Mason <iskanamagus@gmail.com>
2015-06-15 23:17:31 -0700

    Stub git-comment-log

--- bin/git-comment-log.go
+++ bin/git-comment-log.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	// "errors"
+	//gitc "git_comment"
+	kp "gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
 
+var (
+	buildVersion string
+	app          = kp.New("git-comment-log", "List git commit annotations")
+	revision     = app.Arg("revision range", "Filter comments to comments on commits from the specified range").String()
+)
+
+func main() {
+	app.Version(buildVersion)
+	kp.MustParse(app.Parse(os.Args[1:]))
 }
