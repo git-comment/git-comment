c282ec7255032f087c8be030bef2544116430e3c
Delisa Mason <iskanamagus@gmail.com>
2015-06-18 09:42:48 -0700

    Configure pager usage

--- bin/git-comment-log.go
+++ bin/git-comment-log.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	gitc "git_comment"
 	kp "gopkg.in/alecthomas/kingpin.v2"
+	"io"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -25,14 +29,65 @@ func main() {
 	app.FatalIfError(err, errorPrefix)
 	comments, err := gitc.CommentsOnCommit(pwd, revision)
 	app.FatalIfError(err, errorPrefix)
-	// concat all output
-	// check length
-	// check $LINES
-	// if length > $LINES open pager with content
-	// otherwise print content
+	var lineCount int = 0
+	if lineEnv := getEnv("LINES"); lineEnv != nil {
+		lines, _ := strconv.ParseInt(*lineEnv, 10, 0)
+		lineCount = int(lines)
+	}
+	var usePager bool = lineCount == 0
+	var content []byte
+	var writer io.WriteCloser
+	var cmd *exec.Cmd
 	for i := 0; i < len(comments); i++ {
 		comment := comments[i]
-		fmt.Println(*comment.ID)
-		fmt.Println(comment.Serialize())
+		formatted := formattedContent(comment)
+		if !usePager {
+			content = append(content, formatted...)
+			lines := strings.Split(string(content), "\n")
+			usePager = len(lines) > lineCount-1
+		}
+		if usePager {
+			if writer == nil {
+				cmd, writer, err = execPager(pwd)
+				app.FatalIfError(err, errorPrefix)
+			}
+			_, err = writer.Write(formatted)
+			app.FatalIfError(err, errorPrefix)
+		}
 	}
+	if writer != nil {
+		writer.Close()
+		cmd.Wait()
+	} else {
+		fmt.Println(string(content))
+	}
+}
+
+func formattedContent(comment *gitc.Comment) []byte {
+	if comment.ID != nil && len(*comment.ID) > 0 {
+		return []byte(fmt.Sprintf("comment %v\n%v", *comment.ID, comment.Serialize()))
+	}
+	return []byte(comment.Serialize())
+}
+
+func execPager(pwd string) (*exec.Cmd, io.WriteCloser, error) {
+	pager := gitc.ConfiguredPager(pwd)
+	cmd := exec.Command(*pager)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	pipe, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, nil, err
+	}
+	return cmd, pipe, nil
+}
+
+func getEnv(name string) *string {
+	if env := os.Getenv(name); len(env) > 0 {
+		return &env
+	}
+	return nil
 }
