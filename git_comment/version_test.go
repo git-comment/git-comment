aca188bf19286879640d9af2de300b0d02c2ad4b
Delisa Mason <iskanamagus@gmail.com>
2015-07-22 20:11:15 -0700

    Check error messages

--- git_comment/version_test.go
+++ git_comment/version_test.go
@@ -2,6 +2,7 @@ package git_comment
 
 import (
 	"github.com/stvp/assert"
+	"strings"
 	"testing"
 )
 
@@ -13,24 +14,29 @@ func TestCompareVersionsSame(t *testing.T) {
 
 func TestCompareVersionsToolNewer(t *testing.T) {
 	status, err := compareVersion("2.1.1", "2.1.0").Dematerialize()
-	assert.Nil(t, err)
-	assert.Equal(t, status, VersionStatusUpgradeRepo)
+	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), "git-comment --update"))
+	assert.Nil(t, status)
 }
 
 func TestCompareVersionsRepoNewer(t *testing.T) {
 	status, err := compareVersion("2.0.1", "2.1.0").Dematerialize()
-	assert.Nil(t, err)
-	assert.Equal(t, status, VersionStatusUpgradeTool)
+	assert.NotNil(t, err)
+	assert.Nil(t, status)
 }
 
 func TestCompareVersionsToolCorrupted(t *testing.T) {
 	status, err := compareVersion("2..1", "2.1.0").Dematerialize()
 	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), "2..1"))
+	assert.True(t, strings.Contains(err.Error(), "2.1.0"))
 	assert.Nil(t, status)
 }
 
 func TestCompareVersionsRepoCorrupted(t *testing.T) {
 	status, err := compareVersion("1.4.5", "2.0").Dematerialize()
 	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), "1.4.5"))
+	assert.True(t, strings.Contains(err.Error(), "2.0"))
 	assert.Nil(t, status)
 }
